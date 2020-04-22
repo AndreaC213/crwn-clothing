b5559b3c9e31ff8792b4dea96437dcf96fcaf942
Andrea Chang <andrea.chang213@gmail.com>
2020-04-22 15:42:47 -0400

    create cart redux with reducer, types and actions

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,9 @@
+import { CartActionTypes } from './cart.types';
+
+// fire off the an actions holds that snapShot object,
+// we used to store in our state at 'firebase.utils'
+// this action function return objects
+// each object in the format that action expected it to be
+export const toggleCartHidden = () => ({
+    type: CartActionTypes.TOGGLE_CART_HIDDEN,
+});
