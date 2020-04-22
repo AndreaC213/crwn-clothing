520961b6adfe5f6042478d409d71785389ff4c93
Andrea Chang <andrea.chang213@gmail.com>
2020-04-22 13:28:02 -0400

    update our redrict if user is signed in

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,8 +1,10 @@
+import { UserActionTypes } from './user.types';
+
 // fire off the an actions holds that snapShot object,
 // we used to store in our state at 'firebase.utils'
 // this action function return objects
 // each object in the format that action expected it to be
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 });
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from './user.types';
+
 // Initial the state
 const INITIAL_STATE = {
     currentUser: null
@@ -9,7 +11,7 @@ const userReducer = (state= INITIAL_STATE, action) => {
 
     // check the action type
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
