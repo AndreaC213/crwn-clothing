595b0e36bd084d6a231c55b3b409316b53b1698f
Andrea Chang <andrea.chang213@gmail.com>
2020-05-04 04:55:58 -0400

    scaffold the increase and decrease botton in 'checkout-item.component'

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -18,16 +18,20 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
       <img src={imageUrl} alt='item' />
     </div>
     <span className='name'>{name}</span>
-    <span className='quantity'>{quantity}</span>
+    <span className='quantity'>
+      <div className='arrow'>&#10094;</div>
+      <span className='value'>{quantity}</span>
+      <div className='arrow'>&#10095;</div>
+    </span>
     <span className='price'>{price}</span>
-    <span className='remove-botton' onClick={() => clearItem(cartItem)}>
+    <span className='remove-button' onClick={() => clearItem(cartItem)}>
       &#10005;
     </span>
   </div>
 )};
 
 const mapDispatchToProp = dispatch => ({
-  clearItem: Item => dispatch(clearItemFromCart(item))
+  clearItem: item => dispatch(clearItemFromCart(item))
 });
 
 export default connect(null, mapDispatchToProp)(CheckoutItem);
