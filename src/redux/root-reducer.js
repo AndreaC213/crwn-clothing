f4fec7a4c9c3b1585ea6a3ea56e3717ce2778a2f
Andrea Chang <andrea.chang213@gmail.com>
2020-05-04 18:27:55 -0400

    update 'root reducer'with persistence capability

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,25 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist'
+// also get local storage on window browser
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-// reducer key is every slice of state
-export default combineReducers({
+// define new persist config
+// whitelist: contain the string name we want to store
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+// wrape it up with root reducer
+const rootReducer = conbineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+// config by call {persistReducer}
+// get root reducer with the persistence capability
+export default persistReducer(persistConfig, root);
