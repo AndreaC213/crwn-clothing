fc1937cf271906b0f91270a9131e0ef7c6d62af9
Andrea Chang <andrea.chang213@gmail.com>
2020-05-04 04:43:25 -0400

    disptch 'clearItem' from 'cart.reducer' pass as a prop into 'checkout-item component'

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
@@ -7,16 +10,24 @@ import './checkout-item.styles.scss';
 // using UTF-8 char for remove icon
 // passing the whole items as props to 
 // increase nums or decrease
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
   <div className='checkout-item'>
-    <div className='image-container'>
+    <div className='image-container'> 
       <img src={imageUrl} alt='item' />
     </div>
     <span className='name'>{name}</span>
     <span className='quantity'>{quantity}</span>
     <span className='price'>{price}</span>
-    <span className='remove-botton'>&#10005;</span>
+    <span className='remove-botton' onClick={() => clearItem(cartItem)}>
+      &#10005;
+    </span>
   </div>
-);
+)};
 
-export default CheckoutItem;
+const mapDispatchToProp = dispatch => ({
+  clearItem: Item => dispatch(clearItemFromCart(item))
+});
+
+export default connect(null, mapDispatchToProp)(CheckoutItem);
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -3,7 +3,8 @@
 // means define what we want to do to modify that property 'cartItems array'
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-  ADD_ITEM: 'ADD_ITEM'
+  ADD_ITEM: 'ADD_ITEM',
+  CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 };
 
 export default CartActionTypes;
