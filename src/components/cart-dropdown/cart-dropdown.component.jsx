0b180507e1c05ef05cc8cd2607a6dcec6269df0c
Andrea Chang <andrea.chang213@gmail.com>
2020-05-04 02:15:41 -0400

    created checkout page component wireup with 'App.js' then pass the history and location by using {withRouter} in 'cart-dropdown.component'

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -12,7 +13,8 @@ import { selectCartItems } from '../../redux/cart/cart.selectors';
 // step 12.
 // call { cartItems } then it will connect 'cartItems reducer' and 
 // { map } all the current cart item display by { CartItem }
-const CartDropdown = ({ cartItems }) => (
+// ? true : false (also 0: present false in JavaScript)
+const CartDropdown = ({ cartItems, history }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {cartItems.length ? (
@@ -23,7 +25,9 @@ const CartDropdown = ({ cartItems }) => (
                 <span className='empty-message'>Your cart is empty</span>
             )}
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => history.push('./checkout')}>
+            GO TO CHECKOUT
+        </CustomButton>
     </div>
 );
 
@@ -36,4 +40,7 @@ const mapStateToProps = createStructuredSelector({
     cartItems : selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+// wrape {connect} inside {withRouter}
+// passed down the context to all its children
+// which is history and location (path)
+export default withRouter(connect(mapStateToProps)(CartDropdown));
