a7f08e38cdc1dd8b54e06cf36fac905acdee391d
Andrea Chang <andrea.chang213@gmail.com>
2020-05-04 04:01:03 -0400

    wireup cart-items with checkout-items by passing the whole items as props in it to increase or decrease the value

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,16 +4,19 @@ import './checkout-item.styles.scss';
 
 // using image container will be easier to 
 // control the size of it
-// using UTF-8 char for remove
-const CheckoutItem = () => (
+// using UTF-8 char for remove icon
+// passing the whole items as props to 
+// increase nums or decrease
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
   <div className='checkout-item'>
     <div className='image-container'>
-      <img alt='item' />
+      <img src={imageUrl} alt='item' />
     </div>
-    <span className='name'></span>
-    <span className='quantity'></span>
-    <span className='price'></span>
+    <span className='name'>{name}</span>
+    <span className='quantity'>{quantity}</span>
+    <span className='price'>{price}</span>
     <span className='remove-botton'>&#10005;</span>
   </div>
-)
+);
 
+export default CheckoutItem;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
+// pass cartItem as prop into 'CheckoutItem'
 const CheckoutPage = ({ cartItems, total }) => (
   <div className='checkout-page'>
     <div className='checkout-header'>
@@ -26,10 +29,9 @@ const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
     {
-      cartItems.map(cartItem =>
-        cartItem.name
-        )
-    }
+      cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
 
     <div className='total'>
       <span>TOTAL: ${total}</span>
