b1b213937224c1f7bf3f11ff812ca3a6b615c6ae
Andrea Chang <andrea.chang213@gmail.com>
2020-05-04 01:39:50 -0400

    handel cart empty with 'cart-dropdown.component'

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -15,10 +15,13 @@ import { selectCartItems } from '../../redux/cart/cart.selectors';
 const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
-            {
+            {cartItems.length ? (
                 cartItems.map(cartItem => (
-                    <CartItem key={cartItem.id} item={cartItem}/>
-            ))}
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))
+            ) : (
+                <span className='empty-message'>Your cart is empty</span>
+            )}
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
