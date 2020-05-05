8f0a68e3f994de358512f65e4b65d9db1ceff09a
Andrea Chang <andrea.chang213@gmail.com>
2020-05-04 20:48:27 -0400

    implenmenting 'category.component' and import to 'shop.component'

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
 // update shopPage as non-connect component
 // will create child component of the shopPage be connected
-const ShopPage =({ collections }) => (
+// get the current path by passing 'match' the former path as props
+const ShopPage =({ match }) => (
     <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <CollectionsOverview/>
     </div>
 );
