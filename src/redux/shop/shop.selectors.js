6100bf05e6f94ff9487898148df4ff526be06644
Andrea Chang <andrea.chang213@gmail.com>
2020-05-11 04:40:24 -0400

    create new selector 'selectIsCollectionsLoaded' to get the state of the collections is loaded or not

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -26,4 +26,9 @@ export const selectCollection = collectionUrlParam =>
 export const selectIsCollectionsFetching = createSelector(
   [selectShop],
   shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
 )
