1e4f676d23958593fb5228175e920c222f22071e
Andrea Chang <andrea.chang213@gmail.com>
2020-05-12 04:16:49 -0400

    updating 'user.reducer' with the case for actionType and handle the state of erroe

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -2,20 +2,30 @@ import { UserActionTypes } from './user.types';
 
 // Initial the state
 const INITIAL_STATE = {
-    currentUser: null
+    currentUser: null, 
+    error: null
 }
 
 // state would be redux store in whatever action pass in 
 // userReducer with two objects 'state' and 'action'
+// clear the error if the action is successed
 const userReducer = (state= INITIAL_STATE, action) => {
 
     // check the action type
     switch (action.type) {
-        case UserActionTypes.SET_CURRENT_USER:
+        case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
+        case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
             return {
                 ...state,
-                currentUser: action.payload
-            }
+                currentUser: action.payload,
+                error: null
+            };
+        case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
+        case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
         default:
             return state;
     }
