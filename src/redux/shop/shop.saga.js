3bf39a20e1d7e37ce7591fcb5a6203f345892ff6
Andrea Chang <andrea.chang213@gmail.com>
2020-05-12 01:59:39 -0400

    create 'shop.saga' by using 'generator()' and 'takeEvery()'

---
+++ src/redux/shop/shop.saga.js
@@ -0,0 +1,15 @@
+import { takeEvery } from 'redux-saga/effects';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+  yield console.groupCollapsed('I am fired');
+}
+
+// takeEvry is a function for listening the action 
+// second parameter is
+// another generator() will response to current 
+// takeEvery Listener to trigger more code to run
+export function* fetchCollectionsStart() {
+  yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, )
+}
