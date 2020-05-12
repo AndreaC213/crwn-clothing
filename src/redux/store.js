c8d2a26434564918aa1f4b6d955c1cb586e348a4
Andrea Chang <andrea.chang213@gmail.com>
2020-05-12 01:34:46 -0400

    import 'saga' as middleware at 'store.js'

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
 
@@ -10,15 +11,19 @@ import rootReducer from './root-reducer';
 // store is between root-reducer and components
 // so we can pull value from the store to components by usind {provider} in 'index.js'
 
-// pass the middleware as an array to be scalable
-// 'logger' store prevState and nextState 
-const middlewares = [thunk];
+// handling asynchronous event by using saga as middleware
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// sagaMiddleware.run()
 
 // persist current store by using persistor as a provider
 export const persistor = persistStore(store);
