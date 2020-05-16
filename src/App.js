e6f79470de4dec1c9b0fc6b6b1d8f530cea49617
Andrea Chang <andrea.chang213@gmail.com>
2020-05-15 23:22:42 -0400

    updating 'app.component' from a class of method() 'componentDidMount()' to functional component by using 'useEffect()'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -15,49 +15,36 @@ import { GlobalStyle } from './global.styles';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-// how do we confirm the access by google sign in
-// function -> class to acess state
-class App extends React.Component {
-  unsubscribeFromAuth = null
-
-  // Refector the persistence of our user section
-  // destructur 'checkUserSession' from dispatch 
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+// convert class component into functional comoponent by 
+// using 'useEffect' method
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  // want to sign out -> get this subscribe unmount
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
 
-  // pass current state as a property
-  render() {
-    return (
-      <div>
-        <GlobalStyle/>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route exact path='/checkout' component={CheckoutPage}/>
-          <Route 
-            exact 
-            path='/signin' 
-            render={() => 
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInAndSignUpPage/>
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <GlobalStyle/>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route exact path='/checkout' component={CheckoutPage}/>
+        <Route 
+          exact 
+          path='/signin' 
+          render={() => 
+            currentUser ? (
+              <Redirect to='/' />
+            ) : (
+              <SignInAndSignUpPage/>
+            )
+          }
+        />
+      </Switch>
+    </div>
+  );
 }
 
 // getting current user form redux state
