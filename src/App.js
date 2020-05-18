20633bc8e2b350fc105141af9d3fb72cb552d331
Andrea Chang <andrea.chang213@gmail.com>
2020-05-17 20:46:54 -0400

    dynamically pull the state from userContext to 'header.component' to handle the 'props drilling'

--- src/App.js
+++ src/App.js
@@ -31,9 +31,10 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({currentUser: {
-            id: snapShot.id,
-            ...snapShot.data()
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
           }});
         });
       }
